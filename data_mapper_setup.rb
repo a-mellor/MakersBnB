2fe8a66bffa0e11c33005f496e8cb05e12759a8d
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-06-27 15:32:54 +0100

    Tests ability to view a list of spaces. Implements the User model. Add erb view for the list of spaces.

---
+++ data_mapper_setup.rb
@@ -0,0 +1,8 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+require_relative 'models/space'
+
+DataMapper.setup(:default, "postgres://localhost/makersbnb_test")
+DataMapper.finalize
+DataMapper.auto_upgrade!
