50a4035136eaa6338f7ed81b625a2149ce233fdc
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-07-01 14:19:15 +0100

    implements confirm booking requests functionaliy

--- app.rb
+++ app.rb
@@ -104,8 +104,26 @@ class MakersBnB < Sinatra::Base
       space.requests
     end
     @requests_received.flatten!
-
     erb :'requests/requests'
+  end
+
+  get '/requests/review/:request_id' do
+    @request_review = Request.get(params[:request_id])
+    @space = Space.get(@request_review.space_id)
+    @requester = User.get(@request_review.user_id)
+    erb :'requests/review'
+  end
+
+  post '/requests/confirm' do
+    @request_to_confirm = Request.get(params[:request_id])
+    @request_to_confirm.confirmed = true
+    if @request_to_confirm.save
+      redirect '/requests'
+    else
+      flash.keep[:errors] = @request_to_confirm.errors.full_messages
+      redirect '/requests'
+    end
+    redirect '/requests'
   end
 
   run! if app_file == $0
